1f7b1353b6290aa24fa38dfb2a98093f5efec426
ChandniSachdev <chandni1402@gmail.com>
2018-09-10 14:03:14 +0530

    Changes for Exceptions
    Handled Exceptions

--- src/main/java/com/webgmail/automation/Testcases.java
+++ src/main/java/com/webgmail/automation/Testcases.java
@@ -27,14 +27,24 @@ public class Testcases {
     @Test(priority = 1)
     public void browserLaunch() {
 
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\test-output\\chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        log= Logger.getLogger("Window Maximize");
-        driver.get(Constant.url);
-        log= Logger.getLogger("URL Launch");
-    }
+        try {
 
+            System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\test-output\\chromedriver.exe");
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            log = Logger.getLogger("Window Maximize");
+            driver.get(Constant.url);
+            log = Logger.getLogger("URL Launch");
+
+        } catch(Exception e){
+
+            log.error(e);
+            log.debug("Unable to launch URL");
+
+        }
+
+
+    }
     /**
      * This test is Written to login the gmail application
      */
@@ -42,12 +52,20 @@ public class Testcases {
     @Test(priority = 2)
     public void loginMail() {
 
-        driver.findElement((By.id(Constant.Username))).sendKeys("chandni@twojay.co");
-        driver.findElement(By.id(Constant.Next1)).click();
-        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        driver.findElement(By.xpath(Constant.Password)).sendKeys("chandni@1491");
-        driver.findElement(By.xpath(Constant.Next2)).click();
+        try {
+            driver.findElement((By.id(Constant.Username))).sendKeys("chandni@twojay.co");
+            driver.findElement(By.id(Constant.Next1)).click();
+            driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+            driver.findElement(By.xpath(Constant.Password)).sendKeys("chandni@1491");
+            driver.findElement(By.xpath(Constant.Next2)).click();
+
+    }catch (Exception e){
+
+            log.error(e);
+            log.debug("Unable to login");
+        }
     }
+
 
     /**
      * This test is written to logout the gmail application
@@ -56,12 +74,19 @@ public class Testcases {
     @Test(priority = 3)
     public void logoutMail() {
 
+        try{
+
        //driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
        driver.findElement(By.xpath(Constant.ProfileId)).click();
        driver.findElement(By.xpath(Constant.Logout)).click();
 
+    }catch(Exception e){
+
+            log.error(e);
+            log.debug("Unable to logout");
+        }
     }
-}
+ }
 
 
 
